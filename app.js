16a131d4a980e281c5138514b159ea1ab78339cb
Paulo Lacerda <placerda@br.ibm.com>
2016-04-11 13:02:25 -0300

    Initial Commit

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.get('/load', function(request, response) {
 		  for(var i = 0; i < jsonarray.data.length; i++) {
 	 	   //tweaks to store data in cloudant
       		jsonarray.data[i]._id  = "r" + jsonarray.data[i].id;		    
-		    console.log(jsonarray.data[i]._id);
+		    //console.log(jsonarray.data[i]._id);
 		  }
 		bulkSave(jsonarray.data, response);
 	});
